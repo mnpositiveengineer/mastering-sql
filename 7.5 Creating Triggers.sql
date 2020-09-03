f1a4821258d92577987d2df9a5b1a1091a724b5c
Michal Nowak <mnpositiveengineer@gmail.com>
2020-09-03 22:40:08 +0200

    Triggers

--- 7.5 Creating Triggers.sql
+++ 7.5 Creating Triggers.sql
@@ -3,37 +3,62 @@
 */
 use products;
 
--- a) when updating pc price, check if there is no pc with the same or higher speed but lower price
+-- a) when updating pc price, check if there is no pc with higher speed but lower price and no pc with lower speed but higher price
 
 DELIMITER //
 CREATE TRIGGER checkPCprice
 BEFORE UPDATE ON pc
 FOR EACH ROW
 BEGIN
-IF NEW.price > ANY (SELECT price FROM pc WHERE speed >= NEW.speed)
+IF NEW.price > ANY (SELECT price FROM pc WHERE speed > NEW.speed)
+OR NEW.price < ANY (SELECT price FROM pc WHERE speed < NEW.speed)
 THEN 
-set NEW.price = OLD.price;
-set NEW.speed = OLD.speed;
 SIGNAL SQLSTATE '45000' 
 SET MESSAGE_TEXT = 'PC with the same or higher speed cannot have lower price. Update cancelled.';
 END IF;
 END//
 DELIMITER ;
 
--- test1: should not update pc after updating
+DROP TRIGGER checkPCprice;
 
-UPDATE pc SET price = 1000 WHERE model = 1234;
-UPDATE pc SET price = 630 WHERE model = 1012;
-UPDATE pc SET price = 1000 WHERE model IN (1100, 1006);
-UPDATE pc SET price = 1000 WHERE model = 1100;
-UPDATE pc SET price = 1000 WHERE model = 1006;
+-- preparing data
+
+delete from pc;
+delete from products where type = 'pc';
+INSERT INTO products (model, producer, type)
+VALUES (1000, 'A', 'pc'), (1001, 'B', 'pc'), (1002, 'C', 'pc');
+INSERT INTO pc (model, speed, price)
+VALUES (1000, 1, 100), (1001, 2, 200), (1002, 3, 300);
+INSERT INTO products (model, producer, type)
+VALUES (1003, 'D', 'pc'), (1004, 'D', 'pc');
+INSERT INTO pc (model, speed, price)
+VALUES (1003, 4, 400), (1004, 4, 400);
+
+-- test1: should not update pc after updating
+UPDATE pc
+SET price = 301 WHERE model = 1001;
+UPDATE pc
+SET price = 99 WHERE model = 1001;
+
+-- test2: should be able to update pc
+UPDATE pc
+SET price = 300 WHERE model = 1001;
+UPDATE pc
+SET price = 100 WHERE model = 1001;
+UPDATE pc
+SET price = 200 WHERE model = 1001;
+UPDATE pc
+SET price = 500 WHERE model = 1004;
+
+-- test1: should not update pc after updating
+UPDATE pc
+set price = 299 WHERE model = 1004;
+
 
 /* 
 PROBLEM 1 [SOLVED]:
 without DELIMETER command we had a syntax problem:
 Error Code: 1064. You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'END' at line 1
-The DELIMITER command at the beginning of these statements prevents MySQL from processing the trigger definition too soon. 
-The DELIMITER command at the end of these statements returns processing to normal.
 In SQL you close each statement with a delimiter, which is by default a semicolon (;). 
 In a trigger you need to write multiple statements, each ending in a semicolon. 
 To tell MySQL that those semicolons are not the end of your trigger statement, you temporarily change the delimiter from ; to //, 
@@ -49,11 +74,107 @@ Updating the table from a trigger would then cause the same trigger to fire agai
 we have to change action in trigger so it is not update after update ufter update...
 instead of trying to update or delete and insert as an action we changes AFTER to BEFORE and we set NEW.price = OLD.price and NEW.speed = OLD.speed
 
-PROBLEM 4 [UNSOLVED]:
+PROBLEM 4 [SOLVED]:
 UPDATE pc SET price = 1000 WHERE model IN (1100, 1006) --> WE CANNOT EXECUTE SUCH QUERY AS WHEN TRYING TO UPDATE 1100 IT IS 
+We changed the action to:
+
+IF NEW.price > ANY (SELECT price FROM pc WHERE speed > NEW.speed)
+OR NEW.price < ANY (SELECT price FROM pc WHERE speed < NEW.speed)
+
+so that no checkings on records with the same speed are take into consideration
 
 */ 
 
+-- b) while inserting new printer check if in table products such model exists. If not - insert the model into table products.
+
+DELIMITER $$
+CREATE TRIGGER checkPrinterInProducts
+BEFORE INSERT ON printer
+FOR EACH ROW
+BEGIN
+IF NEW.model NOT IN (SELECT model FROM products WHERE type = 'printer')
+THEN
+INSERT INTO products (model, type)
+VALUES (NEW.model, 'printer');
+SIGNAL SQLSTATE '01000'
+SET MESSAGE_TEXT = 'Printer was not included in products table. Updated products table of new printer. Make sure to set producer.';
+END IF;
+END$$
+DELIMITER ;
+
+DROP TRIGGER checkPrinterInProducts;
+
+-- test1: should insert printer into table printer and products
+
+delete from printer;
+delete from products where type = 'printer';
+
+INSERT INTO printer (model)
+VALUES (3000);
+
+/*
+About SIGNALS:
+sqlstate's starting with 45 (SIGNAL SQLSTATE '45000')  are errors and terminate the procedure. 
+sqlstate's starting with 01 (SIGNAL SQLSTATE '01000') are warnings and allow the procedure to continue.
+The warning does not terminate the procedure, and can be seen with SHOW WARNINGS after the procedure returns.
+
+PROBLEM 1 [UNSOLVED]
+
+SIGNAL SQLSTATE '01000' is not displayed on the screen.
+
+*/
+
+-- c) while updating table notebooks, check if average price of notebooks of each producer is less than 1500
+
+DELIMITER %%
+CREATE TRIGGER checkAveragePriceOfNotebooks
+AFTER UPDATE ON notebook
+FOR EACH ROW
+BEGIN
+IF (1500 < ANY (SELECT AVG(price) FROM notebook NATURAL JOIN products GROUP BY producer))
+THEN
+DELETE FROM notebook WHERE model = NEW.model;
+INSERT INTO notebook (model, price) VALUES (OLD.model, OLD.price);
+SIGNAL SQLSTATE '02000' 
+SET MESSAGE_TEXT = 'Average price of notebooks for each producer cannot exceed 1500';
+END IF;
+END %%
+DELIMITER ;
+
+DROP TRIGGER checkAveragePriceOfNotebooks;
+
+-- preparing test data
+delete from notebook;
+delete from products where type = 'notebook';
+
+INSERT INTO products (model, producer, type)
+VALUES 
+(2001, 'A', 'notebook'), 
+(2002, 'A', 'notebook'), 
+(2003, 'B', 'notebook'), 
+(2004, 'B', 'notebook'), 
+(2005, 'C', 'notebook'), 
+(2006, 'C', 'notebook');
+
+INSERT INTO notebook (model, price)
+VALUES
+(2001, 1000),
+(2002, 2000),
+(2003, 1000),
+(2004, 2000),
+(2005, 1000),
+(2006, 2000);
+
+-- test1: should not update price of notebooks
+
+UPDATE notebook
+SET price = 3000 WHERE model = 2002;
+
+/*
+PROBLEM 1 [UNSOLVED]
+
+Error Code: 1442. 
+Can't update table 'notebook' in stored function/trigger because it is already used by statement which invoked this stored function/trigger.
 
 
 
